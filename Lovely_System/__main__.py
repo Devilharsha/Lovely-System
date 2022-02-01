bd5080610d503099ffa2426c124a03de972ccb81
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-01 05:51:54 +0530

    Update __main__.py

--- Lovely_System/__main__.py
+++ Lovely_System/__main__.py
@@ -65,7 +65,7 @@ async def status(event):
     await msg.edit(on_string.format(Enforcer=user_status, name=sender.first_name))
 
 
-@System.on(system_cmd(pattern=r"Lovely stats"))
+@System.on(system_cmd(pattern=r"SENKU stats"))
 async def stats(event):
     msg = f"Processed {System.processed} messages since last restart."
     msg += f"\n{len(ENFORCERS)} Enforcers & {len(INSPECTORS)} Inspectors"
@@ -108,9 +108,9 @@ async def main():
         msg = "Few plugins failed to load:"
         for plugin in FAILED_TO_LOAD:
             msg += f"\n**{plugin}**\n\n`{FAILED_TO_LOAD[plugin]}`"
-        await System.send_message(Lovely_logs, msg)
+        await System.send_message(Senku_logs, msg)
     else:
-        await System.send_message(Lovely_logs, "I'm On Love!")
+        await System.send_message(Senku_logs, "I'm On Love!")
     await System.run_until_disconnected()
 
 
